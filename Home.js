20369590b2f243814b0efba34eae6f7d7d7675ea
vigneshvaranasi <varanasivsv@gmail.com>
2023-12-21 11:01:03 +0530

    Add Smooth Scrolling

--- Home.js
+++ Home.js
@@ -6,3 +6,34 @@ document.addEventListener('DOMContentLoaded', function () {
         collapseElement.classList.toggle('hidden');
     });
 });
+
+function smoothScrolls(target) {
+    const targetElement = document.querySelector(target);
+    const navbarHeight = document.querySelector('.nav').offsetHeight; // Adjust this line with the class or ID of your navbar
+
+    if (targetElement) {
+        const targetPosition = targetElement.offsetTop - navbarHeight;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 800;
+        let start = null;
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+            if (progress < duration) {
+                window.requestAnimationFrame(step);
+            }
+        }
+
+        function easeInOutCubic(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t * t + b;
+            t -= 2;
+            return c / 2 * (t * t * t + 2) + b;
+        }
+
+        window.requestAnimationFrame(step);
+    }
+}
